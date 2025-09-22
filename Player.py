ec06565ac4368410964bda910aadf7315682aa66
Daionyos <tuncsabah.f@gmail.com>
2025-09-22 19:29:05 +0200

    changed main structure

--- Player.py
+++ Player.py
@@ -1,8 +1,6 @@
-
 # Basic shell for the Player class
 class Player:
     def __init__(self):
-        game_score = 0
-        round_score = 0 
-        hand = []
-    pass
+        self.game_score = 0
+        self.round_score = 0 
+        self.hand = []
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from card import Card
+from Player import Player
 import random
 
 def create_deck():
@@ -13,16 +14,16 @@ def create_deck():
 
 def deal_cards(deck):
     random.shuffle(deck)
-    players = [[], [], []]
+    players = [Player(), Player(), Player()]
     for i in range(8):
         for p in range(3):
-            players[p].append(deck.pop())
+            players[p].hand.append(deck.pop())
     table_cards = [deck.pop(), deck.pop()]
     return players, table_cards
 
 if __name__ == "__main__":
     deck = create_deck()
     players, table_cards = deal_cards(deck)
-    for idx, hand in enumerate(players):
-        print(f"Player {idx+1} cards: {hand}")
+    for idx, player in enumerate(players):
+        print(f"Player {idx+1} cards: {player.hand}")
     print(f"Table cards: {table_cards}")
