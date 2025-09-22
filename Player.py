4de60830f48a414f55c69d07e7557a93b439de33
MPukis <91744924+MPukis@users.noreply.github.com>
2025-09-22 19:21:25 +0200

    player

--- Player.py
+++ Player.py
@@ -1,4 +1,8 @@
+
 # Basic shell for the Player class
 class Player:
-	def __init__(self):
-		pass
+    def __init__(self):
+        game_score = 0
+        round_score = 0 
+        hand = []
+    pass
